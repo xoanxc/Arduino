fb15defe956db9e2c779ab3350650dd7ecaa38b9
xoan.xc <61795755+xoanxc@users.noreply.github.com>
2024-05-13 14:28:26 +0200

    Add files via upload

---
+++ Control de Motores con Botones/Control de Motores con Botones.ino
@@ -0,0 +1,39 @@
+// C++ code
+//
+
+int Adelante = 0; //2
+int Izquierda = 0; //1
+int Derecha = 0; //3
+
+void setup()
+{
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  Adelante = digitalRead(2);
+  Izquierda = digitalRead(1);
+  Derecha = digitalRead(3);
+  
+  if (Adelante == HIGH)
+  {
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+  }
+  if (Izquierda == HIGH)
+  {
+    digitalWrite(13, HIGH);
+  }
+  if (Derecha == HIGH)
+  {
+	digitalWrite(12, HIGH);
+  }
+  else
+  {
+    digitalWrite(12, LOW);
+    digitalWrite(13, LOW);
+  }
+  
+}
